b3187fe63f3c67b88fa732c59ed09672216591dd
xumingyu52 <xumingyu52@gmail.com>
2026-03-23 20:20:23 +0800

    add admin

--- blog/blog_context/admin.py
+++ blog/blog_context/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
+from .models import Title, Context, Comment, Like, Collection
 
-# Register your models here.
-from .models import Title, Context
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'date_added', 'text_excerpt')
+    list_filter = ('date_added', 'user')
+    search_fields = ('text', 'user__username', 'post__text')
 
-admin.site.register(Title)
-admin.site.register(Context)
+    def text_excerpt(self, obj):
+        return obj.text[:50] + '...' if len(obj.text) > 50 else obj.text
+    text_excerpt.short_description = '内容摘要'
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('text', 'author', 'date_added')
+    search_fields = ('text', 'author__username')
+
+@admin.register(Context)
+class ContextAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text_excerpt', 'date_added')
+    
+    def text_excerpt(self, obj):
+        return obj.text[:50] + '...' if len(obj.text) > 50 else obj.text
+    text_excerpt.short_description = '内容摘要'
+
+admin.site.register(Like)
+admin.site.register(Collection)
 
